21ca15ce3af82a58d237b840f7badce8bf6bd069
wadd0032 <deannawadden@rogers.com>
2012-03-19 13:55:51 -0400

    deleted js code from this admin. index after confirming with Thomas wasn't needed here

--- admin/index.php
+++ admin/index.php
@@ -37,8 +37,5 @@ $results = $db->query('
 	<?php endforeach; ?>
 	</ul>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-	<script src="../js/open-data-app.js"></script>
-
 </body>
 </html>
