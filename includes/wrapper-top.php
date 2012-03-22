1ad8642d3084b84634c3bcf988a35a4bedf96bc0
wadd0032 <deannawadden@rogers.com>
2012-03-22 14:16:43 -0400

    removed cod for opening/closing of divs; think I want to control this from individual files to assign different ids

--- includes/wrapper-top.php
+++ includes/wrapper-top.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title><?php echo $title; ?> · Ottawa Community Gardens</title>
+	<title><?php if (isset($title)) { echo $title . ' · '; } ?>Ottawa Community Gardens</title>
 	<link href="css/public.css" rel="stylesheet">
 	<link href='http://fonts.googleapis.com/css?family=Princess+Sofia' rel='stylesheet' type='text/css'>
 	<script src="js/modernizr.dev.js"></script>
@@ -13,5 +13,3 @@
 	<header class="masthead">
 		<h1>Ottawa Community Gardens</h1>
 	</header>
-	
-	<div class="main">
