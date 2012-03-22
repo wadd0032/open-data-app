01923e107bfca928e1da59fce9042ad5be939ef3
wadd0032 <deannawadden@rogers.com>
2012-03-22 14:20:13 -0400

    added code to support rating functionality moved placement of code for warpper-top includes to match Thomas' dino example

--- index.php
+++ index.php
@@ -1,36 +1,50 @@
 <?php
 
-$title = 'Home';
-
-include 'includes/wrapper-top.php';
-
 require_once 'includes/db.php';
 
 $results = $db->query('
-	SELECT id, name, street_address, longitude, latitude
+	SELECT id, name, street_address, longitude, latitude,  rate_count, rate_total
 	FROM garden_locations
 	ORDER BY name ASC
 	LIMIT 10
 ');
 
+include_once 'includes/wrapper-top.php';
+
 ?>
 
-
-
-<ul class="garden-list">
+<div id="main-list">
+	<ul class="garden-list">
 	<?php foreach ($results as $garden) : ?>
-		<li itemscope itemtype="http://schema.org/CivicStructure">
+		<?php
+			if ($garden['rate_count'] > 0) {
+				$rating = round($garden['rate_total'] / $garden['rate_count']);
+			} else {
+				$rating = 0;
+			}
+		?>
+		<li itemscope itemtype="http://schema.org/CivicStructure" data-id="<?php echo $garden['id']; ?>">
 			<a href="single.php?id=<?php echo $garden['id']; ?>" itemprop="name"><?php echo $garden['name']; ?></a>
 			<span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
 				<meta itemprop="latitude" content="<?php echo $garden['latitude']; ?>">
 				<meta itemprop="longitude" content="<?php echo $garden['longitude']; ?>">
 			</span>
+			<meter value="<?php echo $rating; ?>" min="0" max="5"><?php echo $rating; ?> out of 5</meter>
+			<ol class="rater">
+			<?php for ($i = 1; $i <= 5; $i++) : ?>
+				<?php $class = ($i <= $rating) ? 'is-rated' : ''; ?>
+				<li class="rater-level <?php echo $class; ?>">★</li>
+			<?php endfor; ?>
+			</ol>
 		</li>
 	<?php endforeach; ?>
-</ul>
+	</ul>
+</div>
 
 <div id="map"></div>
 
 <?php
-	include_once 'includes/wrapper-bottom.php';
+
+include_once 'includes/wrapper-bottom.php';
+
 ?>
