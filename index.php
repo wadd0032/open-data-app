acd205578a98bb961f4b557cf2b9ed9eff4c8399
wadd0032 <deannawadden@rogers.com>
2012-03-29 15:18:43 -0400

    revising code to disply the list/ratings as table

--- index.php
+++ index.php
@@ -6,42 +6,69 @@ $results = $db->query('
 	SELECT id, name, street_address, longitude, latitude,  rate_count, rate_total
 	FROM garden_locations
 	ORDER BY name ASC
-	LIMIT 10
 ');
 
 include_once 'includes/wrapper-top.php';
 
 ?>
+<div class="wrapper">
+	<div id="main-list">
+		<div class="geolocation">
+			<button id="geo">Find Me</button>
+			<p>Enter location</p>
+			<form id="geo-form">
+				<input id="adr" placeholder="Location">
+			</form>
+		</div>
+		
+		<table>
+			<thead>
+				<th>Location</th>
+				<th>Rating</th>
+			</thead>
+			<tbody class="garden-list">
+				
+			<?php foreach ($results as $garden) : ?>
+				<?php
+					if ($garden['rate_count'] > 0) {
+						$rating = round($garden['rate_total'] / $garden['rate_count']);
+					} else {
+						$rating = 0;
+					}
+				?>
 
-<div id="main-list">
-	<ul class="garden-list">
-	<?php foreach ($results as $garden) : ?>
-		<?php
-			if ($garden['rate_count'] > 0) {
-				$rating = round($garden['rate_total'] / $garden['rate_count']);
-			} else {
-				$rating = 0;
-			}
-		?>
-		<li itemscope itemtype="http://schema.org/CivicStructure" data-id="<?php echo $garden['id']; ?>">
-			<a href="single.php?id=<?php echo $garden['id']; ?>" itemprop="name"><?php echo $garden['name']; ?></a>
-			<span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
-				<meta itemprop="latitude" content="<?php echo $garden['latitude']; ?>">
-				<meta itemprop="longitude" content="<?php echo $garden['longitude']; ?>">
-			</span>
-			<meter value="<?php echo $rating; ?>" min="0" max="5"><?php echo $rating; ?> out of 5</meter>
-			<ol class="rater">
-			<?php for ($i = 1; $i <= 5; $i++) : ?>
-				<?php $class = ($i <= $rating) ? 'is-rated' : ''; ?>
-				<li class="rater-level <?php echo $class; ?>">★</li>
-			<?php endfor; ?>
-			</ol>
-		</li>
-	<?php endforeach; ?>
-	</ul>
+				<tr>
+					<td itemscope itemtype="http://schema.org/CivicStructure" data-id="<?php echo $garden['id']; ?>">
+						<a href="single.php?id=<?php echo $garden['id']; ?>" itemprop="name"><?php echo $garden['name']; ?></a>
+						<span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
+							<meta itemprop="latitude" content="<?php echo $garden['latitude']; ?>">
+							<meta itemprop="longitude" content="<?php echo $garden['longitude']; ?>">
+						</span>
+					</td>
+					<td>
+						<ol class="rater">
+							<?php for ($i = 1; $i <= 5; $i++) : ?>
+							<?php $class = ($i <= $rating) ? 'is-rated' : ''; ?>
+							<li class="rater-level <?php echo $class; ?>">★</li>
+							<?php endfor; ?>
+						</ol>
+					</td>
+				</tr>
+				
+				<?php endforeach; ?>
+				
+				</tbody>
+			</table>
+			
+			
+			
+
+
+	</div>
+	
+	<div id="map"></div>
 </div>
 
-<div id="map"></div>
 
 <?php
 
