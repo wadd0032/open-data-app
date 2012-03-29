91803a07cc4073337802a6a94c06ff1119d1c8c4
wadd0032 <deannawadden@rogers.com>
2012-03-29 17:40:59 -0400

    added code for user functionality/security

---
+++ create-user.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'includes/db.php';
+require_once 'includes/users.php';
+
+$email = 'bradlet@algonquincollege.com';
+$password = 'password';
+
+user_create($db, $email, $password);
---
+++ schema/users.sql
@@ -0,0 +1,34 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.3.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 25, 2012 at 08:00 PM
+-- Server version: 5.1.57
+-- PHP Version: 5.3.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `bradlet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
