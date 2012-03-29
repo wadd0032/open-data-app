730661d54e85f87d25069e0850096ebb4149e29f
wadd0032 <deannawadden@rogers.com>
2012-03-29 18:10:33 -0400

    add exception code for IE9 for main-list divas directed by the css gradient generator code

--- includes/wrapper-top.php
+++ includes/wrapper-top.php
@@ -5,6 +5,14 @@
 	<title><?php if (isset($title)) { echo $title . ' · '; } ?>Ottawa Community Gardens</title>
 	<link href="css/public.css" rel="stylesheet">
 	<script src="js/modernizr.dev.js"></script>
+	
+<!--[if gte IE 9]>
+	  <style type="text/css">
+		.gradient {
+		   filter: none;
+		}
+	  </style>
+<![endif]-->
 </head>
 
 <body>
