5344f6d4cb674d4d7f0d0988a33e3e53a5379aac
wadd0032 <deannawadden@rogers.com>
2012-03-29 15:16:38 -0400

    revised to reflect new .js files

--- includes/wrapper-bottom.php
+++ includes/wrapper-bottom.php
@@ -1,6 +1,5 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 	<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyD7ucBegMY84_LCbHdVDeavafwKOBceuAY&sensor=false"></script>
-	<script src="js/garden-map.js"></script>
 	<script src="js/latlng.min.js"></script>
 	<script src="js/open-data-app.js"></script>
 
