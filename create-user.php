8394d3b448b3706257c4434d4b27a2184731e5ae
wadd0032 <deannawadden@rogers.com>
2012-04-23 15:10:28 -0400

    removed create-user.php from public access

--- create-user.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'includes/db.php';
-require_once 'includes/users.php';
-
-$email = 'bradlet@algonquincollege.com';
-$password = 'password';
-
-user_create($db, $email, $password);
